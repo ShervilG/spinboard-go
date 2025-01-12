8a78bb7f29838d2094cb886889877c12ee88afde
ShervilG <wintergpt45@gmail.com>
2025-01-12 19:39:05 +0000

    CHangessss

--- handler.go
+++ handler.go
@@ -3,8 +3,13 @@ package main
 import (
 	"io"
 	"net/http"
+	"time"
 )
 
 func handleHelloWorld(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Spinboard Go says hello !")
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "The time is: "+time.Now().String())
 }
--- main.go
+++ main.go
@@ -4,5 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", handleHelloWorld)
+	http.HandleFunc("/time", timeHandler)
+
 	http.ListenAndServe(":8080", nil)
 }
