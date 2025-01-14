7de2ec75f0772eb5d38beb15f5b4e922e8baf43b
ShervilG <wintergpt45@gmail.com>
2025-01-14 06:47:46 +0000

    Attempted fix

--- discordhandler/cs_handler.go
+++ discordhandler/cs_handler.go
@@ -6,15 +6,24 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var PLAYER_IDS = []string{"344869437750509568", "394094945985757185", "473510771796606996", "534942685610508298"}
+var PLAYER_IDS = []string{"344869437750509568", "394094945985757185", "473510771796606996", "534942685610508298", "694631299323002890"}
 
 func HandleCsAssemble(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
 	if m.Content == "!cs" {
 		message := "Playing CS ?"
 		for _, player := range PLAYER_IDS {
-			message += fmt.Sprintf("<@%s> ", player)
+			if player != m.Author.ID {
+				message += fmt.Sprintf("<@%s> ", player)
+			}
 		}
 
-		_, _ = s.ChannelMessageSend(m.ChannelID, message)
+		_, err := s.ChannelMessageSend(m.ChannelID, message)
+		if err != nil {
+			fmt.Println("Error sending cs assemble message: ", err)
+		}
 	}
 }
